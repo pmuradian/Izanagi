7233524d9e817a056c2affdea1f3da72a1fcc272
Paruyr <p.muradian@gmail.com>
2019-09-14 01:40:29 +0400

    Add response statuses

--- src/main/java/main/controllers/UserController.java
+++ src/main/java/main/controllers/UserController.java
@@ -1,6 +1,7 @@
 package main.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import main.services.*;
 
@@ -10,21 +11,25 @@ public class UserController {
     private UserService userService;
 
     @PostMapping(value = "/users")
+    @ResponseStatus(HttpStatus.CREATED)
     public User createUser(@RequestBody UserSpec userSpec) {
         return userService.createUser(userSpec);
     }
 
     @DeleteMapping(value = "/users/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Boolean deleteUser(@PathVariable String id) {
         return userService.deleteUser(id);
     }
 
     @GetMapping(value = "/users/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public User getUser(@PathVariable String id) {
         return userService.getUser(id);
     }
 
     @PatchMapping(value = "/users/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public User updateUser(@PathVariable String id, @RequestBody UserSpec userSpec) {
         return userService.updateUser(id, userSpec);
     }
