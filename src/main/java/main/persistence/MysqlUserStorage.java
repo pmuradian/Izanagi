2253d170d622d5a12859a86fcd2828813b106793
Paruyr <p.muradian@gmail.com>
2019-09-14 01:28:24 +0400

    Implement methods to get, update and delete user in database

--- src/main/java/main/persistence/MysqlUserStorage.java
+++ src/main/java/main/persistence/MysqlUserStorage.java
@@ -18,22 +18,14 @@ public class MysqlUserStorage {
          Connection connection = null;
 
          try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb","root","");
+             Class.forName("com.mysql.cj.jdbc.Driver");
+             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "root", "");
          } catch (ClassNotFoundException | SQLException ex) {
              logger.log(Level.SEVERE, "Unable to obtain a connection", ex);
          }
 
          return connection;
      }
-
-//     private void closeConnection(Connection connection) {
-//         try {
-//             connection.close();
-//         } catch (Exception ex) {
-//             logger.log(Level.SEVERE, "Unable to close a connection");
-//         }
-//     }
 
     public UserEntity store(UserSpec userSpec) {
         UserEntity userEntity = null;
@@ -57,18 +49,52 @@ public class MysqlUserStorage {
     }
 
     public UserEntity get(String id) {
-         return null;
-//         return storage.get(id);
+        UserEntity userEntity = null;
+        try (Connection connection = getConnection()) {
+            PreparedStatement selectStatement = connection.prepareStatement("select * from users where id = ?;");
+            selectStatement.setString(1, id);
+            ResultSet resultSet = selectStatement.executeQuery();
+            if (resultSet.next()) {
+                UserSpec userSpec = new UserSpec(resultSet.getString(2),
+                                                 resultSet.getString(3),
+                                                 resultSet.getString(4));
+                userEntity = new UserEntity(id, userSpec);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return userEntity;
     }
 
     public Boolean delete(String id) {
-         return null;
-//        return storage.remove(id) != null;
+        try (Connection connection = getConnection()) {
+            PreparedStatement selectStatement = connection.prepareStatement("delete from users where id = ?;");
+            selectStatement.setString(1, id);
+            Integer result = selectStatement.executeUpdate();
+            return result > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     public UserEntity update(String id, UserSpec userSpec) {
-        UserEntity userEntity = new UserEntity(id, userSpec);
-//        storage.put(id, userEntity);
+        UserEntity userEntity = null;
+        try (Connection connection = getConnection()) {
+            PreparedStatement insertStatement = connection.prepareStatement("update users set login = ?, password = ?, email = ? where id = ?;");
+            UserEntity userToInsert = new UserEntity(id, userSpec);
+            insertStatement.setString(1, userToInsert.getLogin());
+            insertStatement.setString(2, userToInsert.getPassword());
+            insertStatement.setString(3, userToInsert.getEmail());
+            insertStatement.setString(4, userToInsert.getId());
+
+            Integer isInserted = insertStatement.executeUpdate();
+            if (isInserted > 0) {
+                userEntity = userToInsert;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return userEntity;
     }
 }
