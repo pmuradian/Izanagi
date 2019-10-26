93dc35a9149aa2d5bec1d4f124339bcf1732a73a
Paruyr <p.muradian@gmail.com>
2019-10-26 22:50:56 +0400

    Change enum values

--- src/main/java/main/specs/RatingType.java
+++ src/main/java/main/specs/RatingType.java
@@ -1,14 +1,14 @@
 package main.specs;
 
 public enum RatingType {
-    LIKE(1),
-    DISLIKE(-1),
-    NONE(0);
+    LIKE(0),
+    NONE(1),
+    DISLIKE(2);
 
     public static RatingType fromValue(int rawValue) {
-        if (rawValue == 1) return LIKE;
-        if (rawValue == -1) return DISLIKE;
-        if (rawValue == 0) return NONE;
+        if (rawValue == 0) return LIKE;
+        if (rawValue == 1) return NONE;
+        if (rawValue == 2) return DISLIKE;
         return NONE;
     }
 
