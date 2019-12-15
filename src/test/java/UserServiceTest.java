54dfb126935171ce99b3c103ccc20e45521ff844
Paruyr <p.muradian@gmail.com>
2019-12-15 16:24:55 +0400

    Add tests

---
+++ src/test/java/UserServiceTest.java
@@ -0,0 +1,58 @@
+import main.models.Result;
+import main.models.User;
+import main.persistence.MysqlStorage;
+import main.persistence.MysqlUserStorage;
+import main.services.UserService;
+import main.services.StatusCodes;
+import main.specs.UserSpec;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {UserService.class, MysqlUserStorage.class})
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UserServiceTest {
+    @Autowired
+    private UserService userService;
+
+    private static String userLogin = "rick1";
+    private static String userPassword = "password1";
+    private static String userEmail = "rick1@citadel.mul";
+    private static String userID;
+
+    @BeforeAll
+    public void setup() {
+        UserSpec userSpec = new UserSpec(userLogin, userPassword, userEmail);
+        Result<User> result = userService.createUser(userSpec);
+        User user = result.getValue();
+        if (result.getStatusCode() != StatusCodes.OK || user == null) {
+            throw new IllegalStateException(result.getStatusMessage());
+        }
+    }
+
+    @AfterAll
+    public void teardown() {
+        Result<Boolean> result = userService.deleteUser(userID);
+        Boolean isRemoved = result.getValue();
+        if (result.getStatusCode() != StatusCodes.OK || isRemoved == false) {
+            throw new IllegalStateException(result.getStatusMessage());
+        }
+    }
+
+    @Test
+    public void testGetUsersByValidCredentials() {
+        Result<User> result = userService.getUserByCredentials(userLogin, userPassword);
+        assertEquals(StatusCodes.OK, result.getStatusCode());
+    }
+
+    @Test
+    public void whenUserDoesNotExists_shouldFailWithIllegalStateException() {
+        assertEquals(1, 1);
+    }
+}
